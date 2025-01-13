962054c62c51d5f0ac06a51a016b16dff17fe400
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-01-13 23:27:40 +0100

    Refactor and BugFix: Refactored the code to make it more readable and modular. In addition fixed bug with Freestyle drawing when Menu component got focus.

--- MyPaint/MainWindow.xaml.cs
+++ MyPaint/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace MyPaint
     {
         Freestyle,
         Point,
-        Line,
+        Segment,
         Rectangle,
         Polygon
     }
@@ -28,157 +28,154 @@ namespace MyPaint
     public partial class MainWindow : Window
     {
         private DrawStyle _drawStyle = DrawStyle.Freestyle;
-
-        private Point _currentPoint = new();
+        private Point _currentMousePosition = new();
         private Color _currentColor = Color.FromRgb(0, 0, 0); //Black color by default
+
+        private bool _isMenuFocused = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            //if (e.ButtonState == MouseButtonState.Pressed)
-            //{
-            //   e.GetPosition(this);
-            //}
-        }
 
+
+
+        // ==================================================
+        // PRIVATE EVENT METHODS
+        // ==================================================
+
+        // !!! MOUSE EVENTS !!! //
         private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed && _drawStyle == DrawStyle.Freestyle)
             {
-                var brushColor = new SolidColorBrush(_currentColor);
-                var line = new Line
+                if (_isMenuFocused)
                 {
-                    X1 = _currentPoint.X,
-                    Y1 = _currentPoint.Y,
-                    X2 = e.GetPosition(this).X,
-                    Y2 = e.GetPosition(this).Y,
-                    Stroke = brushColor
-                };
+                    FocusManager.SetFocusedElement(FocusManager.GetFocusScope(menu), null); //Removes the focus from "menu".
+                    return;
+                }
 
-                _currentPoint = e.GetPosition(this);
+                var brushColor = new SolidColorBrush(_currentColor);
+                var line = DrawManager.DrawLine(_currentMousePosition, e.GetPosition(this), brushColor);
 
-                MainCanvas.Children.Add(line);
+                _currentMousePosition = e.GetPosition(this);
+
+                mainCanvas.Children.Add(line);
             }
         }
 
         private void MainCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            _currentPoint = e.GetPosition(this);
+            _currentMousePosition = e.GetPosition(this);
 
-            if (_drawStyle == DrawStyle.Point)
+            switch(_drawStyle)
             {
-                const double width = 6;
-                const double height = 6;
-                var brushColor = new SolidColorBrush(_currentColor);
+                case DrawStyle.Point:
+                    AddPointAndMakeVisible();
+                    break;
+                case DrawStyle.Segment:
 
-                var ellipse = new Ellipse
-                {
-                    Width = width,
-                    Height = height,
-                    Fill = brushColor
-                };
-
-                Canvas.SetLeft(ellipse, e.GetPosition(this).X - ellipse.Width / 2);
-                Canvas.SetTop(ellipse, e.GetPosition(this).Y - ellipse.Height / 2);
-
-                MainCanvas.Children.Add(ellipse);
-            }
-            else if (_drawStyle == DrawStyle.Rectangle)
-            {
-                const double width = 60;
-                const double height = 40;
-                var brushColor = new SolidColorBrush(_currentColor);
-                var rectangle = new Rectangle
-                {
-                    Width = width,
-                    Height = height,
-                    Stroke = brushColor
-                };
-
-                Canvas.SetLeft(rectangle, e.GetPosition(this).X - rectangle.Width / 2);
-                Canvas.SetTop(rectangle, e.GetPosition(this).Y - rectangle.Height / 2);
-
-                MainCanvas.Children.Add(rectangle);
-            }
-            else if (_drawStyle == DrawStyle.Polygon)
-            {
-                var mouseX = e.GetPosition(this).X;
-                var mouseY = e.GetPosition(this).Y;
-                double polygonSize = 20.0;
-
-                var point1 = new Point(mouseX - polygonSize, mouseY + polygonSize * 2);
-                var point2 = new Point(mouseX + polygonSize, mouseY + polygonSize * 2);
-                var point3 = new Point(mouseX + polygonSize * 2, mouseY + 0);
-                var point4 = new Point(mouseX + polygonSize, mouseY - polygonSize * 2);
-                var point5 = new Point(mouseX - polygonSize, mouseY - polygonSize * 2);
-                var point6 = new Point(mouseX - polygonSize * 2, mouseY + 0);
-
-                var polyglonPoints = new PointCollection
-                {
-                    point1,
-                    point2,
-                    point3,
-                    point4,
-                    point5,
-                    point6
-                };
-
-                var brushColor = new SolidColorBrush(_currentColor);
-
-                var polygon = new Polygon
-                {
-                    Points = polyglonPoints,
-                    Stroke = brushColor
-                };
-
-                MainCanvas.Children.Add(polygon);
+                    break;
+                case DrawStyle.Rectangle:
+                    AddRectangleAndMakeVisible();
+                    break;
+                case DrawStyle.Polygon:
+                    AddPolygonAndMakeVisible();
+                    break;
             }
         }
 
-        private void buttonDraw_Click(object sender, RoutedEventArgs e)
+        // !!! MENU EVENTS !!! //
+        private void Menu_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _isMenuFocused = true;
+        }
+
+        private void Menu_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _isMenuFocused = false;
+        }
+
+        // !!! CHANGE DRAW STYLE EVENTS !!! //
+        private void DrawFreestyleButton_Click(object sender, RoutedEventArgs e)
         {
             _drawStyle = DrawStyle.Freestyle;
         }
 
-        private void buttonPoints_Click(object sender, RoutedEventArgs e)
+        private void DrawPointsButton_Click(object sender, RoutedEventArgs e)
         {
             _drawStyle = DrawStyle.Point;
         }
 
-        private void buttonLines_Click(object sender, RoutedEventArgs e)
+        private void DrawSegmentMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            _drawStyle = DrawStyle.Line;
+            _drawStyle = DrawStyle.Segment;
         }
 
-        private void drawSegment_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void editSegment_Click(object sender, RoutedEventArgs e)
+        private void EditSegmentMenuItem_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void drawRect_Click(object sender, RoutedEventArgs e)
+        private void DrawRectangleMenuItem_Click(object sender, RoutedEventArgs e)
         {
             _drawStyle = DrawStyle.Rectangle;
         }
 
-        private void editPolygon_Click(object sender, RoutedEventArgs e)
+        private void DrawPolygonMenuItem_Click(object sender, RoutedEventArgs e)
         {
             _drawStyle = DrawStyle.Polygon;
         }
 
-        private void colorPicker_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+
+        // !!! NEW WINDOW EVENTS !!! //
+        private void ColorPickerRectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var colorPickerWindow = new ColorPickerWindow();
 
             colorPickerWindow.Show();
+        }
+
+
+
+
+        // =========================================================
+        // PRIVATE HELPFUL METHODS
+        // =========================================================
+        private void AddPointAndMakeVisible()
+        {
+            const double width = 6d;
+            const double height = 6d;
+            var brushColor = new SolidColorBrush(_currentColor);
+            var ellipse = DrawManager.DrawEllipse(width, height, brushColor);
+
+            Canvas.SetLeft(ellipse, _currentMousePosition.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, _currentMousePosition.Y - ellipse.Height / 2);
+
+            mainCanvas.Children.Add(ellipse);
+        }
+
+        private void AddRectangleAndMakeVisible()
+        {
+            const double width = 60d;
+            const double height = 40d;
+            var brushColor = new SolidColorBrush(_currentColor);
+            var rectangle = DrawManager.DrawRectangle(width, height, brushColor);
+
+            Canvas.SetLeft(rectangle, _currentMousePosition.X - rectangle.Width / 2);
+            Canvas.SetTop(rectangle, _currentMousePosition.Y - rectangle.Height / 2);
+
+            mainCanvas.Children.Add(rectangle);
+        }
+
+        private void AddPolygonAndMakeVisible()
+        {
+            const double polygonSize = 20d;
+            var brushColor = new SolidColorBrush(_currentColor);
+            var polygon = DrawManager.DrawPolygon(_currentMousePosition, polygonSize, brushColor);
+
+            mainCanvas.Children.Add(polygon);
         }
     }
 }
